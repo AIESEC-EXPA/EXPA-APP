b365392672cf8b298da35e4b7a45e55d0be9da6a
FanQuan <fanquan@FanQuandeMac-mini.local>
2014-12-30 23:44:36 +0800

    Initial Commit

---
+++ EXPA-APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EXPA-APP
+//
+//  Created by FanQuan on 12/30/14.
+//  Copyright (c) 2014 AIESEC-EXPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
