eb04f75ff2fb078abec96a8b9f3b734fd527a614
FanQuan <fanquan@FanQuandeMac-mini.local>
2015-01-05 23:04:27 +0800

    JSON

--- EXPA-APP/ViewController.swift
+++ EXPA-APP/ViewController.swift
@@ -12,18 +12,25 @@ import SwiftHTTP
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.Label1.text = "bbbbbbbc"
         var request = HTTPTask()
         request.responseSerializer = JSONResponseSerializer()
-        request.GET("https://gis-api.aiesec.org:443/v1/current_person.json", parameters: ["access_token":"740e53727c7826245ee470e1482e45dec765308774e7a283b8a06cfbcdd39fd1"],
+        request.GET("https://gis-api.aiesec.org:443/v1/current_person.json", parameters: ["access_token":"f267b2c69766072c63ce112329fc25c76737f39dc51aea9328e7886406d1d01c"],
             success: {(response: HTTPResponse) in
-                if let data = response.responseObject as? NSData {
-                    let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+                if let dict = response.responseObject as? Dictionary<String, AnyObject> {
+                    self.Label1.text = "cccc"
+                    
+                    println("example of the JSON key: \(dict)")
+                    
+                    
+                    
+                    
                     //NSJSONSerializa
-                    println("response: \(str)")
+                    //println("response: \(str)")
                 }
             },
             failure: {(error: NSError, response: HTTPResponse?) in
