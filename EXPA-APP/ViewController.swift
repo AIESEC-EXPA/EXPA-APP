9f4e6bd87e0273a85f53b12e9e24ac6642b2187d
FanQuan <fanquan@FanQuandeMac-mini.local>
2015-01-06 00:23:27 +0800

    JSON enabled

--- EXPA-APP/ViewController.swift
+++ EXPA-APP/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 import SwiftHTTP
+//import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -18,20 +19,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.Label1.text = "bbbbbbbc"
         var request = HTTPTask()
-        request.responseSerializer = JSONResponseSerializer()
+        //request.responseSerializer = JSONResponseSerializer()
         request.GET("https://gis-api.aiesec.org:443/v1/current_person.json", parameters: ["access_token":"f267b2c69766072c63ce112329fc25c76737f39dc51aea9328e7886406d1d01c"],
             success: {(response: HTTPResponse) in
-                if let dict = response.responseObject as? Dictionary<String, AnyObject> {
-                    self.Label1.text = "cccc"
-                    
-                    println("example of the JSON key: \(dict)")
-                    
-                    
-                    
-                    
-                    //NSJSONSerializa
-                    //println("response: \(str)")
+                if let data = response.responseObject as? NSData {
+                    let json = JSON(data: data)
+                    println(json["person"]["email"])
+                    self.Label1.text = json["person"]["email"].stringValue
                 }
+                
             },
             failure: {(error: NSError, response: HTTPResponse?) in
                     println("error: \(error)")
